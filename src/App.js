ab01afb786b39d92b92c181572aa805efe6b2528
Saif Maknojia <saif.maknojia@outlook.com>
2021-07-16 23:08:48 -0400

    working on form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-
 import DollarSign from './assets/images/icon-dollar.svg';
 import PersonIcon from './assets/images/icon-person.svg';
 import TipsFormContainer from './component/TipsFormContainer/TipsFormContainer';
 import InputForm from './component/inputForm/InputForm';
 import AmountCount from './component/amountCount/AmountCount';
 import BrandImage from './component/brandImage/BrandImage';
+
 function App() {
+  const [input, setInput] = useState({
+    bill: '',
+    numberOfPeople: ''
+  });
+  console.log('our Total bill is: ', input.bill);
+  console.log('Total number of people is :', input.numberOfPeople);
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setInput(prev => {
+      return {
+        ...prev,
+        [name]: value
+      };
+    });
+  };
+
+  const handleReset = () => {
+    setInput({ bill: '', numberOfPeople: '' });
+  };
+
   return (
     <div className="container">
       <div className="main-container">
         <BrandImage />
         <div className="main">
           <div className="custom">
-            <InputForm name="bill" img={DollarSign} title="Bill" />
+            <InputForm
+              placeholder="0.00"
+              name="bill"
+              value={input.bill}
+              handleChange={handleChange}
+              img={DollarSign}
+              title="Bill"
+            />
             <TipsFormContainer />
             <InputForm
-              name="people"
+              value={input.numberOfPeople}
+              handleChange={handleChange}
+              placeholder="Total Number of people"
+              name="numberOfPeople"
               img={PersonIcon}
               title="Number of People"
             />
@@ -29,7 +60,9 @@ function App() {
                 <AmountCount title="Tip Amount" amount="4.72" />
                 <AmountCount title="Total Amount" amount="572.00" />
               </div>
-              <button className="reset__button">Reset</button>
+              <button onClick={handleReset} className="reset__button">
+                Reset
+              </button>
             </div>
           </div>
         </div>
--- src/component/inputForm/InputForm.js
+++ src/component/inputForm/InputForm.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
-const InputForm = ({ img, title, name }) => {
+const InputForm = ({ img, title, name, placeholder, value, handleChange }) => {
   return (
     <div className="bill">
       <h2 className="bill__header">{title}</h2>
       <div className="bill__value">
         <img className="bill__value--dollar" src={img} alt="value" />
-        <input className="bill__value--input" type="number" name={name} />
+        <input
+          placeholder={placeholder}
+          onChange={handleChange}
+          className="bill__value--input"
+          value={value}
+          type="number"
+          name={name}
+        />
       </div>
     </div>
   );
