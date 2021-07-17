51c49380f10864f646c4948ab56422b5069187f8
Saif Maknojia <saif.maknojia@outlook.com>
2021-07-16 22:03:26 -0400

    working on css for form-inputs

--- src/component/inputForm/InputForm.js
+++ src/component/inputForm/InputForm.js
@@ -6,7 +6,7 @@ const InputForm = ({ img, title, name }) => {
       <h2 className="bill__header">{title}</h2>
       <div className="bill__value">
         <img className="bill__value--dollar" src={img} alt="value" />
-        <input className="bill__value--input" type="text" name={name} />
+        <input className="bill__value--input" type="number" name={name} />
       </div>
     </div>
   );
